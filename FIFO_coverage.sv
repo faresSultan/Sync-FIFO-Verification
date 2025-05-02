ddcebe1f69fcb43075ae6df85669df5d9dcbb482
faresSultan <fares.sultan9@gmail.com>
2025-05-02 15:43:16 +0300

    Functional cover points

--- FIFO_coverage.sv
+++ FIFO_coverage.sv
@@ -1,8 +1,34 @@
 package FIFO_coverage_pkg;
+    import FIFO_transaction_pkg::*;
 
     class FIFO_coverage;
 
-        
+        FIFO_transaction F_cvg_txn;
+
+        function void new();
+            FIFO_coverage = new;
+        endfunction
+
+        covergroup FIFO_coverage;
+            
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.full;  
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.almostfull;
+
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.empty;  
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.almostempty;  
+
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.overflow;  
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.underflow;  
+
+            cross F_cvg_txn.wr_en , F_cvg_txn.rd_en , F_cvg_txn.wr_ack;  
+
+        endgroup
+
+        function void sample_data(FIFO_transaction F_txn);
+            F_cvg_txn = new F_txn;
+            FIFO_coverage.sample();
+        endfunction
+
         
     endclass
 endpackage
