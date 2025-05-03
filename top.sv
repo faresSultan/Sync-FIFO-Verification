ebc655327bb3706deecb5a5363d31fafbaa3a717
faresSultan <fares.sultan9@gmail.com>
2025-05-03 18:14:07 +0300

    async reset assertion

--- top.sv
+++ top.sv
@@ -4,8 +4,17 @@ module top();
     always #2 clk = ~clk;
 
     FIFO_IF fifo_if (clk);
-
     FIFO DUT (fifo_if);
     TestBench tb (fifo_if);
-    FIFO_monitor monitor (fifo_if); 
+    FIFO_monitor monitor (fifo_if);
+
+    always_comb begin
+        if(!fifo_if.rst_n) begin 
+            Reset: assert final (
+                !(fifo_if.wr_ack || fifo_if.underflow || fifo_if.overflow || fifo_if.almostempty||
+                    fifo_if.almostfull || fifo_if.full || fifo_if.data_out) && fifo_if.empty
+            )
+            else $fatal("Assertion Reset failed!");
+        end
+    end
 endmodule
